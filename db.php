68d61446e46ed926fa3b403cddb76107f2c2c7a2
VoThuyLinh <72687362+volinh911@users.noreply.github.com>
2021-06-08 16:03:02 +0700

    db for web

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$user = "id16991579_19bit2_letters";
+$pw = "TGA#r^^q{(s6H~Ap";
+$db = "id16991579_letters";
+
+// #1: create the conn object
+$conn = new mysqli($host, $user, $pw, $db);
