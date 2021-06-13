f2c2246b3bc3778ba691a35c76fa13fc37cf66ea
VoThuyLinh <72687362+volinh911@users.noreply.github.com>
2021-06-13 09:56:04 +0700

    new header, footer and example

---
+++ example.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+    <!--using FontAwesome--------------->
+    <script crossorigin="anonymous" src="https://kit.fontawesome.com/c8e4d183c2.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- ===== CSS ===== -->
+    <link rel="stylesheet" href="css/sub_header_footer_styles.css">
+
+    <title>Letters</title>
+    <link rel="shortcut icon" href="images/favicon_io/favicon.ico">
+</head>
+
+<body>
+    <?php
+    include 'includes/sub_header.php'
+    ?>
+
+    <div class="containter my-5 pb-5 pt-5">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, non. Deserunt, nesciunt cupiditate excepturi deleniti incidunt exercitationem assumenda in recusandae, temporibus autem iure aspernatur id quos libero soluta et fugiat.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptatum fugiat, corrupti modi autem, rem distinctio odio ipsam consequuntur dolorem ullam quasi ipsum consequatur ea soluta repudiandae nobis totam vel?</p>
+    </div>
+
+    <?php
+    include 'includes/sub_footer.php'
+    ?>
+
+    <script src="js/sub_header.js"></script>
+</body>
+
+</html>
---
+++ includes/sub_header.php
@@ -0,0 +1,13 @@
+
+    <!--===== HEADER =====-->
+    <header id="header">
+        <a href="#" class="logo">Letters</a>
+        <div class="toggle"></div>
+        <ul class="navigation">
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="example.php">Scrambles</a></li>
+            <li><a href="#">Community</a></li>
+            <li><a href="#">Sign In</a></li>
+            <li><a href="#">Sign Up</a></li>
+        </ul>
+    </header>
