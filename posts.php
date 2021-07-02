49ceb060dfd5de666f237c92224e8d276dc8c752
llgarlic <73431572+llgarlic@users.noreply.github.com>
2021-07-02 18:36:27 +0700

    add a very very simple post.php uwu

---
+++ posts.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+  <!--using FontAwesome--------------->
+  <script crossorigin="anonymous" src="https://kit.fontawesome.com/c8e4d183c2.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <title>Document</title>
+  <link rel="stylesheet" href="css/posts.css">
+  <link rel="stylesheet" href="css/sub_header_footer_styles.css">
+  <link rel="shortcut icon" href="images/favicon_io/favicon.ico">
+</head>
+
+
+<body>
+  <?php include_once('./includes/sub_header.php'); ?>
+  <div class="container-fluid page-wrapper">
+
+    <!-- forum titles placeholder -->
+    <div class="container">
+
+      <div class="table-row">
+        <div class="subjects">
+          <a href="">Is learning Python on 2021 worth it?</a>
+          <br>
+          <span>Started by <b><a href="">User</a></b> .</span>
+        </div>
+        <div class="replies">
+          2 replies <br> 125 views
+        </div>
+
+        <div class="last-reply">
+          Oct 12 2021
+          <br>By <b><a href="">User</a></b>
+        </div>
+      </div>
+
+      <div class="table-row">
+        <div class="subjects">
+          <a href="">Is learning Python on 2021 worth it?</a>
+          <br>
+          <span>Started by <b><a href="">User</a></b> .</span>
+        </div>
+        <div class="replies">
+          2 replies <br> 125 views
+        </div>
+
+        <div class="last-reply">
+          Oct 12 2021
+          <br>By <b><a href="">User</a></b>
+        </div>
+      </div>
+
+    </div>
+    <!---->
+  </div>
+  </div>
+
+
+
+
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  <?php include_once('./includes/sub_footer.php'); ?>
+
+</body>
+
+</html>
