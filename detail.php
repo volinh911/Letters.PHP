b5fb7c13cf78b27c579e8422c5295baf1ba99ce5
llgarlic <73431572+llgarlic@users.noreply.github.com>
2021-07-05 23:19:02 +0700

    update detail.php

--- detail.php
+++ detail.php
@@ -1,67 +1,67 @@
 <?php
 
-    include_once ('./path.php');
-    include_once (ROOT_PATH . '/controllers/posts.php');
-    $post_id = $_GET["id"];
+include_once('./path.php');
+include_once(ROOT_PATH . '/controllers/posts.php');
+$post_id = $_GET["id"];
 
-    if (isset($_SESSION["user_id"])) {
-        $user_id = $_SESSION["user_id"];
-    }
-    
-    // Get post qua id
-    $posts = new Post($conn);
-    $post = $posts->getPost($post_id);
-    var_dump($post);
-    // Dem post roi luu vao bien, bien day van la array
-    $totalPost = $posts->countPost($post["user_id"]);
+if (isset($_SESSION["user_id"])) {
+    $user_id = $_SESSION["user_id"];
+}
 
-    // Lay comment
-    $comments = new Comment($post_id,$conn);
-    $userComments= $comments->showComments();
+// Get post qua id
+$posts = new Post($conn);
+$post = $posts->getPost($post_id);
 
-    // Add comment
-    if (isset($_POST["add-comment"])) {
-        $comment_content = $_POST["comment"];
-        $comment = $comments->checkComment($user_id, $post_id, $comment_content);
-        $errors = $comments->errors;
-    }
+// Dem post roi luu vao bien, bien day van la array
+$totalPost = $posts->countPost($post["user_id"]);
 
-    // Edit comment
-    $commentRows = "";
-    $update = false;    
-    if (isset($_POST["edit-comment"])) {
-        $update = true;
-        $id = $_POST["edit-comment"];
-        $_SESSION["comment-id"] = $id;
-        $commentRow = $comments->getComment($id); 
-        $commentRows = $commentRow["content"]; //Tach array
-    } 
+// Lay comment
+$comments = new Comment($post_id, $conn);
+$userComments = $comments->showComments();
 
-    if(isset($_POST["update-comment"])){
-        $editCommentID = $_SESSION["comment-id"];
-        // var_dump($editCommentID);
-        $comment_content = $_POST["comment"];
-        // var_dump($comment_content);
-        $updated_comment = $comments->editComment($editCommentID, $comment_content);
-    }
+// Add comment
+if (isset($_POST["add-comment"])) {
+    $comment_content = $_POST["comment"];
+    $comment = $comments->checkComment($user_id, $post_id, $comment_content);
+    $errors = $comments->errors;
+}
 
-    // Delete comment
-    if (isset($_POST["delete-comment"])) {
-        $deleteCommentID = $_POST["delete-comment"];
-        $comments->deleteComment($deleteCommentID);
-    } 
-      
-    // Delete post
-    if (isset($_POST["delete-post"])) {
-        $posts->deletePost($post_id);
-    } 
+// Edit comment
+$commentRows = "";
+$update = false;
+if (isset($_POST["edit-comment"])) {
+    $update = true;
+    $id = $_POST["edit-comment"];
+    $_SESSION["comment-id"] = $id;
+    $commentRow = $comments->getComment($id);
+    $commentRows = $commentRow["content"]; //Tach array
+}
 
-    // Edit Post
-    if(isset($_POST['edit-post'])){
-        $_SESSION['post_id']= $post_id;
-        $_SESSION['edit']= "1";
-        header("Location: create.php");
-    }
+if (isset($_POST["update-comment"])) {
+    $editCommentID = $_SESSION["comment-id"];
+    // var_dump($editCommentID);
+    $comment_content = $_POST["comment"];
+    // var_dump($comment_content);
+    $updated_comment = $comments->editComment($editCommentID, $comment_content);
+}
+
+// Delete comment
+if (isset($_POST["delete-comment"])) {
+    $deleteCommentID = $_POST["delete-comment"];
+    $comments->deleteComment($deleteCommentID);
+}
+
+// Delete post
+if (isset($_POST["delete-post"])) {
+    $posts->deletePost($post_id);
+}
+
+// Edit Post
+if (isset($_POST['edit-post'])) {
+    $_SESSION['post_id'] = $post_id;
+    $_SESSION['edit'] = "1";
+    header("Location: create.php");
+}
 
 ?>
 
@@ -83,8 +83,7 @@
 
     <!--using FontAwesome--------------->
     <script crossorigin="anonymous" src="https://kit.fontawesome.com/c8e4d183c2.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
     <title>Document</title>
     <link rel="stylesheet" href="css/community.css">
@@ -92,139 +91,148 @@
     <link rel="shortcut icon" href="images/favicon_io/favicon.ico">
 </head>
 
-<body>
+<body class="detail-wrapper">
 
-
-    <?php include_once('./includes/sub_header.php'); ?>
+    <section class="detail-header"> <?php include_once('./includes/sub_header.php'); ?>
+    </section>
 
     <div class="container page-wrapper">
         <!--Navigation-->
-        <div class="div m-3">
-            <a href="posts.php">Back to Posts</a>
-            <hr>
+        <div class="div m-3 redirect">
+            <a href="posts.php">
+                << Back to Posts</a>
+                    <hr>
         </div>
 
         <!--Topic Section-->
-        <div class="topic-container">
+        <div class="topic-container p-5">
             <!--Original thread-->
             <div class="head">
                 <div class="authors">Author</div>
                 <div class="content">Topic: <?php echo $post["title"]; ?></div>
                 <!-- Neu nguoi dung hien tai == author cua post thi duoc edit/delete -->
-                <?php if (isset($_SESSION["user_id"])): ?>
-                <?php if($user_id == $post['user_id'] || $_SESSION["admin"] == 1): ?>
+                <?php if (isset($_SESSION["user_id"])) : ?>
+                    <?php if ($user_id == $post['user_id'] || $_SESSION["admin"] == 1) : ?>
 
-                <form action="detail.php?id=<?php echo $post_id;?>" method="POST">
-                    <button type="submit" name="delete-post">Delete
-                    </button>
+                        <form action="detail.php?id=<?php echo $post_id; ?>" method="POST">
+                            <button type="submit" name="delete-post">Delete
+                            </button>
 
-                    <a href="create.php"><button type="submit" name="edit-post">Edit
-                    </button></a>
-                </form>
-                
-                <?php endif; ?>
+                            <a href="create.php"><button type="submit" name="edit-post">Edit
+                                </button></a>
+                        </form>
+
                     <?php endif; ?>
+                <?php endif; ?>
             </div>
-
+            <hr>
             <div class="body">
                 <div class="authors">
                     <div class="username"><a href=""><?php echo $post["username"]; ?></a></div>
                     <!-- Role admin/user -->
-                    <div>Role: <?php if($post["admin"] == 0):echo "Normal User"; else: echo "Admin"; endif;?></div>
+                    <div>Role: <?php if ($post["admin"] == 0) : echo "Normal User";
+                                else : echo "Admin";
+                                endif; ?></div>
                     <!-- Khong profile image thi hinh mac dinh -->
-                    <img src="<?php if ($post['image_profile'] != null): echo $post['image_profile']; else: echo "https://cdn.pixabay.com/photo/2015/11/06/13/27/ninja-1027877_960_720.jpg"; endif; ?>" alt=""
-                        id="avatar">
-                        <!-- Dem post Author -->
+                    <img src="<?php if ($post['image_profile'] != null) : echo $post['image_profile'];
+                                else : echo "https://cdn.pixabay.com/photo/2015/11/06/13/27/ninja-1027877_960_720.jpg";
+                                endif; ?>" alt="" id="avatar">
+                    <!-- Dem post Author -->
                     <div>Number of Posts: <u><?php echo $totalPost['id'];  ?></u></div>
                 </div>
 
                 <!-- Noi dung post -->
                 <div class="content">
                     <?php echo $post["content"]; ?> <br>
-                    <img src="<?php echo $post['image'];?>" alt="">
+                    <img src="<?php echo $post['image']; ?>" alt="">
                 </div>
             </div>
         </div>
 
         <!--Comment Area-->
-        <?php if (isset($_SESSION["user_id"])):?>
-        <form action="detail.php?id=<?php echo $post_id;?>" method="POST">
-            <div class="comment-area" id="comment-area">
-                <!-- Neu edit se dua noi dung truoc khi sua cho nguoi dung -->
-                <textarea name="comment" id="" placeholder="comment here ... "><?php echo $commentRows;?></textarea>
-                <!-- Neu khong phai sua thi comment bth -->
-                <?php if($update == false): ?>
-                <button type="submit" name="add-comment">Submit </button>
-                <?php else: ?>
-                <button type="submit" name="update-comment">Update </button>
-                <?php endif; ?>
-            </div>
-        </form>
+        <?php if (isset($_SESSION["user_id"])) : ?>
+            <form action="detail.php?id=<?php echo $post_id; ?>" method="POST">
+                <div class="comment-area" id="comment-area">
+                    <!-- Neu edit se dua noi dung truoc khi sua cho nguoi dung -->
+                    <textarea name="comment" id="" placeholder="comment here ... "><?php echo $commentRows; ?></textarea>
+                    <!-- Neu khong phai sua thi comment bth -->
+                    <?php if ($update == false) : ?>
+                        <button type="submit" name="add-comment">Submit </button>
+                    <?php else : ?>
+                        <button type="submit" name="update-comment">Update </button>
+                    <?php endif; ?>
+                </div>
+            </form>
 
-        <!-- Khong dang nhap khong duoc comment -->
-        <?php else: ?>
-        <div class="mt-5 col-md-6 text-center">
-            <h2 class="display-5">Please Login to Comment!</h2>
-            <p>Create an account or login to post to the website.</p>
-            <button type="button" class="btn btn-block btn-outline-primary"><a href="login.php"><i
-                        class="fas fa-sign-in-alt"></i> Create Account/Login</a> </button>
-        </div>
+            <!-- Khong dang nhap khong duoc comment -->
+        <?php else : ?>
+            <div class="row justify-content-center">
+                <div class="my-5 p-4 col-md-6 text-center login-request">
+                    <h2 class="display-5">Please Login to Comment!</h2>
+                    <p>Create an account or login to post to the website.</p>
+                    <button type="button" class="btn btn-block btn-outline-primary"><a href="login.php"><i class="fas fa-sign-in-alt"></i> Create Account/Login</a> </button>
+                </div>
+            </div>
         <?php endif; ?>
 
         <!--Comments Section-->
         <!-- In comment -->
-        <?php foreach($userComments as $userComment):?>
-        <div class="container comments py-5">
-            <div class="body">
-                <div class="authors">
-                    <div class="username"><a href=""><?php echo $userComment['username']; ?></a></div>
-                    <!-- Role admin/user -->
-                    <div>Role <?php if ($userComment['admin'] == 0): echo "Normal User"; else: echo "Admin"; endif; ?>
+        <?php foreach ($userComments as $userComment) : ?>
+            <div class="container comments py-5">
+                <div class="body">
+                    <div class="authors">
+                        <div class="username"><a href=""><?php echo $userComment['username']; ?></a></div>
+                        <!-- Role admin/user -->
+                        <div>Role <?php if ($userComment['admin'] == 0) : echo "Normal User";
+                                    else : echo "Admin";
+                                    endif; ?>
+                        </div>
+                        <!-- Khong hinh = hinh mac dinh -->
+                        <img src="<?php if ($userComment['image_profile'] != null) : echo $userComment['image_profile'];
+                                    else : echo "https://cdn.pixabay.com/photo/2015/11/06/13/27/ninja-1027877_960_720.jpg";
+                                    endif; ?>" alt="" id="avatar">
+                        <!-- Dem posts cua nguoi dung -->
+                        <!-- Neu chua post gi se tra ra null->loi nen set = 0 -->
+                        <!-- Lay value tu key -->
+                        <div>Number of Posts:
+                            <?php if ($posts->countPost($userComment['user_id']) < 1) : echo "0";
+                            else : echo implode($posts->countPost($userComment['user_id']));
+                            endif; ?>
+                            <u></u>
+                        </div>
                     </div>
-                    <!-- Khong hinh = hinh mac dinh -->
-                    <img src="<?php if ($userComment['image_profile'] != null): echo $userComment['image_profile']; else: echo "https://cdn.pixabay.com/photo/2015/11/06/13/27/ninja-1027877_960_720.jpg"; endif;?>"
-                        alt="" id="avatar">
-                    <!-- Dem posts cua nguoi dung -->
-                    <!-- Neu chua post gi se tra ra null->loi nen set = 0 -->
-                    <!-- Lay value tu key -->
-                    <div>Number of Posts:
-                        <?php if($posts->countPost($userComment['user_id']) < 1): echo "0"; else: echo implode($posts->countPost($userComment['user_id'])); endif; ?>
-                        <u></u>
+                    <div class="content">
+                        <!-- Noi dung comment -->
+                        <?php echo $userComment["content"]; ?>
+                        <!-- Neu nguoi dang loggedin == nguoi viet comment hoac la admin thi duoc edit/delete comment -->
+                        <?php if (isset($_SESSION["user_id"])) : ?>
+                            <?php if ($user_id == $userComment['user_id'] || $_SESSION["admin"] == 1) : ?>
+
+                                <form action="detail.php?id=<?php echo $post_id; ?>" method="POST">
+                                    <button type="submit" value="<?php echo $userComment["comment_id"]; ?>" name="delete-comment">Delete </button>
+
+                                    <button type="submit" value="<?php echo $userComment["comment_id"]; ?>" name="edit-comment">Edit </button>
+                                </form>
+
+                            <?php endif; ?>
+                        <?php endif; ?>
                     </div>
                 </div>
-                <div class="content">
-                <!-- Noi dung comment -->
-                    <?php echo $userComment["content"]; ?>
-                    <!-- Neu nguoi dang loggedin == nguoi viet comment hoac la admin thi duoc edit/delete comment -->
-                    <?php if (isset($_SESSION["user_id"])): ?>
-                    <?php if($user_id == $userComment['user_id'] || $_SESSION["admin"] == 1): ?>
+                <hr>
 
-                    <form action="detail.php?id=<?php echo $post_id;?>" method="POST">
-                        <button type="submit" value="<?php echo $userComment["comment_id"]; ?>"
-                            name="delete-comment">Delete </button>
-
-                        <button type="submit" value="<?php echo $userComment["comment_id"]; ?>"
-                            name="edit-comment">Edit </button>
-                    </form>
-
-                    <?php endif; ?>
-                    <?php endif; ?>
-                </div>
             </div>
-            <hr>
-
-        </div>
         <?php endforeach; ?>
 
     </div>
 
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
 
-    <?php include_once('./includes/sub_footer.php'); ?>
+    <section class="detail-footer">
+        <?php include_once('./includes/sub_footer.php'); ?>
+    </section>
 
     <!-- Script for header -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
